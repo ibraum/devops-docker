5835dbf2c758beda92975f3adcaff28e63bd348d
ibraum <ibraumodnok@gmail.com>
2025-12-06 14:58:43 +0000

    K8S

--- kubernetes-demo/index.js
+++ kubernetes-demo/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello from Kubernetes Demo!");
+  res.send({
+    message: "Hello from Kubernetes Demo!",
+    service: "kubernetes-demo",
+    pod: process.env.POD_NAME || "unknown",
+    time: new Date().toISOString(),
+  });
 });
-const PORT = process.env.PORT || 3000;
+
+app.get("/readyz", (req, res) => { res.status(200).send("ready"); });
+
+app.get("/healthz", (req, res) => { res.status(200).send("OK"); });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
